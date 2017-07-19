d24cfbc7866faf2a4830289433ac0eb26ac85ccc
pc <pc@192.168.0.102>
2017-07-19 20:45:34 +0200

    Initi comming

--- autoload.php
+++ autoload.php
@@ -5,7 +5,7 @@
  */
 
 spl_autoload_register(function ($class) {
-    $file = __DIR__ . "/" . str_replace('\\', '/', $class) . '.php';
+    $file = __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
     if (file_exists($file)) {
         require $file;
     }
